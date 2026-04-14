a88f4fde61c4ab8852c81eb2af0f38edf8c9940e
boaca926 <bo.cao@physics.uu.se>
2026-04-14 10:43:27 +0200

    header_bdt

--- header_bdt/path_sample.h
+++ header_bdt/path_sample.h
@@ -1,3 +1,3 @@
-const TString data_type = "sig";
-const TString sampleFile = "/home/bo/Desktop/analysis_root_v6/sig";
-const TString outputCut = "../../test_samples.root";
+const TString data_type = "eeg";
+const TString sampleFile = "../../input_vertex_TDATA_bkgrej/input/eeg";
+const TString outputCut = "../../kloe_sample_chain.root";
