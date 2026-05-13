8e88904a87b0f26d72fcffd94c67fd83998f1321
KLOE <caobo875@gmail.com>
2026-05-13 11:26:52 +0200

    all changes

--- bdt/main_initialize_kloe_opti.py
+++ bdt/main_initialize_kloe_opti.py
@@ -33,7 +33,7 @@ uv run main_initialize_kloe_opti.py \
     --input /home/kloe/Desktop/KLOE_BDT/dataset/kloe_bdt.root \
     --chunk-size 50000 \
     --output-dir /home/kloe/Desktop/KLOE_BDT/dataset_bdt
-# alternative input data at: /home/kloe/Desktop/input_bdt_TDATA_chain/cut/tree_pre.root (make sure cuts are removed)
+# alternative input data at: /home/kloe/Desktop/input_kloe_TDATA_chain/cut/tree_pre.root (make sure cuts are removed)
 """
 
 # ========== ADDED: Cut constants (matching C++ header_bdt/cut_para.h) ==========
