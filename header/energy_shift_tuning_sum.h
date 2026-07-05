4e0cf7951b8f7a0293a4f3e3ffb8a8ab76107502
bo <caobo875@gmail.com>
2026-07-05 08:37:47 +0200

    all changes

--- header/energy_shift_tuning_sum.h
+++ header/energy_shift_tuning_sum.h
@@ -1,12 +1,12 @@
-const double energy_shift0 = -0.28989; // iteration 1; 
-const double energy_shift0_err = 0.0223559;
+const double energy_shift0 = 3.0679; // iteration 1; massbias_tuning.h
+const double energy_shift0_err = 0.0224552;
 
 const double energy_shift_total = energy_shift0;
 const double energy_shift_total_err = TMath::Sqrt(TMath::Power(energy_shift0_err, 2));
 
 double alpha_delta = energy_shift_total / 353.36;   // Sum of all iterations
-//const double MASS_SCALE_PI0 = 1 + alpha_delta;
-const double MASS_SCALE_PI0 = 1.; // Set to determine mass bias from pull tuning
+const double MASS_SCALE_PI0 = 1 + alpha_delta;
+//const double MASS_SCALE_PI0 = 1.; // Set to determine mass bias from pull tuning
 
 // from tuning_raw.h, data pulls
 const double bias_E12 = -0.0794267;
--- header/massbias_tuning.h
+++ header/massbias_tuning.h
@@ -1,3 +1,3 @@
-const double energy_shift = 3.11486;
-const double energy_shift_err = 0.0747229;
-const double mass_bias_Z = 41.6855;
+const double energy_shift = 3.0679;
+const double energy_shift_err = 0.0224552;
+const double mass_bias_Z = 136.623;
